1076fc4376d3d1cd0f53784ef19da1d6d9f3034d
omarsalem1 <39171000+omarsalem1@users.noreply.github.com>
2021-06-15 11:58:31 +0200

    addedModels&EF

---
+++ Migrations/20210615095724_addednotnull.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Technical_Task_Enozom.Migrations
+{
+    public partial class addednotnull : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
